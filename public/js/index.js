43f3abf9350ab3095818bdbbbb628a79adf2c107
Seung-hwan285 <ghks285@naver.com>
2022-02-24 11:43:46 +0900

    chore : 노드설치 및 디렉터리 변경

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+import {onClientSlider, onHomeSlider} from "./handle/onHomeSlider.js";
+import {onHomeScroll} from "./handle/onHomeScroll.js";
+import {$} from "./utils/querySelector.js";
+
+import {onConstEmailSend} from "./handle/onContact.js";
+
+const App=()=>{
+
+
+    window.addEventListener('scroll',onHomeScroll);
+    onClientSlider();
+    onHomeSlider();
+
+    $('.btn').addEventListener("click",onConstEmailSend);
+
+}
+
+window.onload=()=>{
+    App();
+}
+
