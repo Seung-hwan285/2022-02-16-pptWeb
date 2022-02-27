e50256acd0850719c46d7fb947a3b8740e5a81ac
Seung-hwan285 <ghks285@naver.com>
2022-02-27 13:30:47 +0900

    reafctor : 반응형 웹으로 리펙토링

--- public/js/handle/onContact.js
+++ public/js/handle/onContact.js
@@ -4,11 +4,5 @@ export const onContact=(e)=>{
         console.log(e.target.files[0]);
         console.log(e.target.files[0].name);
 
-        const js={
-                name : 'fdasf',
-        };
-
-        console.log(js.name);
-
 
 }
--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@ const App=()=>{
 
     window.addEventListener('scroll',onHomeScroll);
     onClientSlider();
-    // onHomeSlider();
+    onHomeSlider();
 
 
 
