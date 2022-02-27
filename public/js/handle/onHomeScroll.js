7b0c92ad7481869130cda937de7e9413edb72192
Seung-hwan285 <ghks285@naver.com>
2022-02-27 16:34:57 +0900

    fix : 스크롤내리면 메뉴바 엑세스 안되는 현상

--- public/js/handle/onHomeScroll.js
+++ public/js/handle/onHomeScroll.js
@@ -3,7 +3,7 @@ import {$} from "../utils/querySelector.js";
 export  const onHomeScroll=()=>{
 
         const $header = $('header');
-
+        console.log(window.scrollY);
         $header.classList.toggle('sticky',window.scrollY >0);
 
 }
